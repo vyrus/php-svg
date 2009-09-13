136a8872687cb80cccb084b1663cba3cd3046070
Vyrus <piqlet@gmail.com>
2009-09-13 22:00:38 +0400

    [+] Отступы при генерации кода.

--- index.php
+++ index.php
@@ -1,24 +1,29 @@
 <?php
 
     class SVG_Abstract {
-        //const INDENT_SIZE = 4;
+        const INDENT_SIZE = 4;
         
         protected $_attributes = array();
         
         protected $_elements = array();
         
-        protected function _renderElements() {
+        protected function _renderElements($indent_level) {
             $code = null;
             
             foreach ($this->_elements as $elem) {
-                $code .= $elem->render();
+                $code .= $elem->render($indent_level);
             }
             
             return $code;
         }
         
-        protected function _renderElement($tag, $attrs = array(), $content = null, $force_content = false) {
-            $elem = '<';
+        protected function _renderElement($tag, $attrs = array(), $content = null, $indent_level, $force_content = false) {
+            $elem = PHP_EOL;
+            
+            $indent = $indent_level * self::INDENT_SIZE;
+            $elem .= str_repeat(' ', $indent);
+            
+            $elem .= '<';
             $elem .= $tag;
             
             if (sizeof($attrs))
@@ -32,9 +37,13 @@
             if (null !== $content || $force_content) {
                 $elem .= '>';
                 $elem .= $content;
+                
+                $elem .= PHP_EOL;
+                
+                $elem .= str_repeat(' ', $indent);
                 $elem .= '</' . $tag . '>';
             } else {
-                $elem .= '/>';
+                $elem .= ' />';
             }
             
             return $elem;
@@ -75,9 +84,8 @@
         
         protected function _render() {
             $svg = '<?xml version="1.0" encoding="utf-8" standalone="yes"?>';
-            $svg .= PHP_EOL;
             
-            $content = $this->_renderElements();
+            $content = $this->_renderElements(1);
             
             $attrs = array(
                 'version'     => '1.1',
@@ -88,7 +96,7 @@
                 'height'      => '400px',
                 'width'       => '400px'
             );
-            $svg .= $this->_renderElement('svg', $attrs, $content, true);
+            $svg .= $this->_renderElement('svg', $attrs, $content, 0, true);
             
             return $svg;
         }
@@ -101,9 +109,9 @@
             $this->_tag = $tag;
         }
         
-        public function render() {
-            $content = $this->_renderElements();
-            return $this->_renderElement($this->_tag, $this->_attributes, $content);
+        public function render($indent_level) {
+            $content = $this->_renderElements($indent_level + 1);
+            return $this->_renderElement($this->_tag, $this->_attributes, $content, $indent_level);
         }
     }
     
